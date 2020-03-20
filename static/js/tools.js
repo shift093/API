2cd8f4f0ecde581c8ea140f7acd7f7141d1baafc
Setup <33972291+shift093@users.noreply.github.com>
2020-03-20 15:11:21 +0800

    Delete tools.js

--- static/js/tools.js
+++
@@ -1,8 +0,0 @@
-// Function to get variables in a url
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-}
