a0036d6dc394db78600d2b7e6aad95cf4896d8db
Setup <33972291+shift093@users.noreply.github.com>
2020-03-20 15:18:26 +0800

    Create data.js

---
+++ static/js/benchmarks/data.js
@@ -0,0 +1,21 @@
+var data = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': '/static/js/benchmarks/data.json',
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+
+// remove some rows under non-lambda mode
+if (lambda_mode === 0){
+    data = $.grep(data, function(n, i){
+      var select = (n.Lambda === 0);
+      return select;
+    })
+}
